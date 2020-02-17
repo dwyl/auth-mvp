3c065c87a989bf93d3c4e1b6e70139a843a55e1a
SimonLab <simon.labondance@gmail.com>
2020-02-17 17:20:37 +0100

    remove typo in prod.config, #1

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :auth_mvp, AuthMvpWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "auth-mvp.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
