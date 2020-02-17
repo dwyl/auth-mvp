72d883cbb74cef428be14d570885b09119554fb6
SimonLab <simon.labondance@gmail.com>
2020-02-17 17:51:13 +0100

    use elixir_auth_google 1.1, https://github.com/dwyl/auth-mvp/issues/1#issuecomment-587079789

--- lib/auth_mvp_web/controllers/auth_url_controller.ex
+++ lib/auth_mvp_web/controllers/auth_url_controller.ex
@@ -3,7 +3,7 @@ defmodule AuthMvpWeb.AuthUrlController do
 
   def index(conn, _params) do
     referer = get_referer(conn)
-    google_url = ElixirAuthGoogle.generate_oauth_url(conn) <> "&state=#{referer}"
+    google_url = ElixirAuthGoogle.generate_oauth_url(conn, referer)
     github_url = ElixirAuthGithub.login_url(%{scope: ["user:email"], state: referer})
 
     urls = [%{url: google_url, type: "google"}, %{url: github_url, type: "github"}]
