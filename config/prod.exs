0b55f083dd96de81805663ed44aa30999b4cbfa1
SimonLab <simon.labondance@gmail.com>
2020-02-17 17:01:53 +0100

    prepare app to be able to deployed on heroku, #1

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :auth_mvp, AuthMvpWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "auth-mvp.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
 
 # Do not print debug messages in production
 config :logger, level: :info
--- lib/auth_mvp_web/endpoint.ex
+++ lib/auth_mvp_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule AuthMvpWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :auth_mvp
 
   socket "/socket", AuthMvpWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
