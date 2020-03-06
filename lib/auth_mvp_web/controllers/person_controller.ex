ee12548ed3da40ff5bf0c1f44577ccacea799d10
SimonLab <simon.labondance@gmail.com>
2020-03-06 15:04:12 +0100

    send back id_person for /person/info endpoint, https://github.com/dwyl/auth-mvp/issues/7#issuecomment-595781357

--- lib/auth_mvp_web/controllers/person_controller.ex
+++ lib/auth_mvp_web/controllers/person_controller.ex
@@ -4,7 +4,7 @@ defmodule AuthMvpWeb.PersonController do
   def index(conn, _params) do
     person = AuthMvp.People.get_person_by_email(conn.assigns.claims.email)
 
-    render(conn, "index.json", person: %{email: person.email})
+    render(conn, "index.json", person: %{id_person: person.id, email: person.email})
   end
 
 end
