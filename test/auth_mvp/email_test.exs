31e68710439d851941abfa03069dbff668dfda31
nelsonic <nelson+github@dwyl.io>
2020-03-27 16:56:45 +0000

    implement sendemail/1 function for https://github.com/dwyl/auth/issues/41

--- test/auth_mvp/email_test.exs
+++ test/auth_mvp/email_test.exs
@@ -1,29 +1,17 @@
 defmodule AuthMvp.EmailTest do
   use ExUnit.Case
-  # alias AuthMvp.People.Person
+
   describe "AuthMvp.Email" do
-    test "send/1 an email" do
+    test "sendemail/1 an email" do
       params = %{
         "email" => "success@simulator.amazonses.com",
         "name" => "Super Successful",
         "template" => "welcome"
       }
-      IO.inspect(params, label: "params")
-      res = AuthMvp.Email.send(params)
-      IO.inspect(res, label: "res")
+      # IO.inspect(params, label: "params")
+      res = AuthMvp.Email.sendemail(params)
+      assert Map.get(params, "email") == Map.get(res, "email")
+      assert Map.get(res, "id") > 0
     end
   end
-
-  # test "Get /person/info returns person email when request header authorization is correct" do
-  #   assert {:ok, %Person{} = person} = AuthMvp.People.create_person(%{email: "person@dwyl.com"})
-  #   jwt = AuthMvp.Token.generate_and_sign!(%{email: "person@dwyl.com"})
-  #
-  #   conn =
-  #     build_conn()
-  #     |> put_req_header("authorization", "Bearer #{jwt}")
-  #     |> get("/person/info")
-  #
-  #   response = json_response(conn, 200)
-  #   assert response["data"]["email"] == "person@dwyl.com"
-  # end
 end
