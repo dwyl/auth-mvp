04f87bfd50be979d2afd893da365ebf1393879f4
SimonLab <simon.labondance@gmail.com>
2020-04-01 15:45:58 +0100

    create html page for oauth, #19

--- lib/auth_mvp_web/controllers/auth_url_controller.ex
+++ lib/auth_mvp_web/controllers/auth_url_controller.ex
@@ -17,8 +17,7 @@ defmodule AuthMvpWeb.AuthUrlController do
     google_url = @elixir_auth_google.generate_oauth_url(conn, referer)
     github_url = @elixir_auth_github.login_url(%{scope: ["user:email"], state: referer})
 
-    urls = [%{url: google_url, type: "google"}, %{url: github_url, type: "github"}]
-    render(conn, "index.html", urls: urls)
+    render(conn, "index.html", oauth_google_url: google_url, oauth_github_url: github_url)
   end
 
   defp get_referer(conn) do
