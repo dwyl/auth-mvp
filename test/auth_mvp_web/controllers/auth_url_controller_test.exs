5e218542c0c0219bd0020add523a296f4da07773
SimonLab <simon.labondance@gmail.com>
2020-04-01 16:09:52 +0100

    update test fo auth controller, #19

--- test/auth_mvp_web/controllers/auth_url_controller_test.exs
+++ test/auth_mvp_web/controllers/auth_url_controller_test.exs
@@ -5,11 +5,11 @@ defmodule AuthMvpWeb.AuthUrlControllerTest do
     conn = conn
            |> put_req_header("referer", "http://locahost:4000/")
            |> get("/auth/urls")
-    assert json_response(conn, 200)
+    assert html_response(conn, 200)
   end
 
   test "GET /auth/urls", %{conn: conn} do
     conn = get(conn, "/auth/urls")
-    assert json_response(conn, 200)
+    assert html_response(conn, 200)
   end
 end
