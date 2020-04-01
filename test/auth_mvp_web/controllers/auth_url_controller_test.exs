10e37e60cadf80ac0b64b3a18211530cffdd879d
SimonLab <simon.labondance@gmail.com>
2020-04-01 16:48:08 +0100

    use accept header for test api request, #19

--- test/auth_mvp_web/controllers/auth_url_controller_test.exs
+++ test/auth_mvp_web/controllers/auth_url_controller_test.exs
@@ -2,9 +2,11 @@ defmodule AuthMvpWeb.AuthUrlControllerTest do
   use AuthMvpWeb.ConnCase
 
   test "GET /auth/urls with referer", %{conn: conn} do
-    conn = conn
-           |> put_req_header("referer", "http://locahost:4000/")
-           |> get("/auth/urls")
+    conn =
+      conn
+      |> put_req_header("referer", "http://locahost:4000/")
+      |> get("/auth/urls")
+
     assert html_response(conn, 200)
   end
 
@@ -14,17 +16,21 @@ defmodule AuthMvpWeb.AuthUrlControllerTest do
   end
 
   test "GET /auth/urls with referer using api", %{conn: conn} do
-    conn = conn
-           |> put_req_header("referer", "http://locahost:4000/")
-           |> put_resp_content_type("application/json")
-           |> get("/auth/urls")
+    conn =
+      conn
+      |> put_req_header("referer", "http://locahost:4000/")
+      |> put_req_header("accept", "application/json")
+      |> get("/auth/urls")
+
     assert json_response(conn, 200)
   end
 
   test "GET /auth/urls using api", %{conn: conn} do
-    conn = conn
-           |> put_resp_content_type("application/json")
-           |> get("/auth/urls")
+    conn =
+      conn
+      |> put_req_header("accept", "application/json")
+      |> get("/auth/urls")
+
     assert json_response(conn, 200)
   end
 end
