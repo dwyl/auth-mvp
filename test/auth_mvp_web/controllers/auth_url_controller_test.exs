c3fa7cdf497609f5a0481d47c63f7e295eb72ec1
SimonLab <simon.labondance@gmail.com>
2020-04-01 16:18:08 +0100

    add test for auth url api, #19

--- test/auth_mvp_web/controllers/auth_url_controller_test.exs
+++ test/auth_mvp_web/controllers/auth_url_controller_test.exs
@@ -12,4 +12,19 @@ defmodule AuthMvpWeb.AuthUrlControllerTest do
     conn = get(conn, "/auth/urls")
     assert html_response(conn, 200)
   end
+
+  test "GET /auth/urls with referer using api", %{conn: conn} do
+    conn = conn
+           |> put_req_header("referer", "http://locahost:4000/")
+           |> put_req_header("content-type", "application/json")
+           |> get("/auth/urls")
+    assert json_response(conn, 200)
+  end
+
+  test "GET /auth/urls using api", %{conn: conn} do
+    conn = conn
+           |> put_req_header("content-type", "application/json")
+           |> get("/auth/urls")
+    assert json_response(conn, 200)
+  end
 end
