4340ec37e0a843392f6bf6d5d719dca800491ba1
SimonLab <simon.labondance@gmail.com>
2020-04-15 15:44:27 +0100

    update profile avatar on login, #24

--- lib/auth_mvp_web/controllers/github_auth_controller.ex
+++ lib/auth_mvp_web/controllers/github_auth_controller.ex
@@ -6,17 +6,30 @@ defmodule AuthMvpWeb.GithubAuthController do
   """
   def index(conn, %{"code" => code, "state" => client}) do
     {:ok, profile} = ElixirAuthGithub.github_auth(code)
-    {:ok, session} = case AuthMvp.People.get_person_by_email(profile.email) do
-      nil ->
-        {:ok, person} = AuthMvp.People.create_person(%{email: profile.email, avatar: profile.avatar_url})
-        AuthMvp.Email.sendemail(%{email: profile.email, template: "welcome"})
-        AuthMvp.People.create_session(person)
 
-      person ->
-        AuthMvp.People.create_session(person)
-    end
+    {:ok, session} =
+      case AuthMvp.People.get_person_by_email(profile.email) do
+        nil ->
+          {:ok, person} =
+            AuthMvp.People.create_person(%{email: profile.email, avatar: profile.avatar_url})
 
-    jwt = AuthMvp.Token.generate_and_sign!(%{email: profile.email, session: session.id, avatar: profile.avatar})
+          # AuthMvp.Email.sendemail(%{email: profile.email, template: "welcome"})
+          AuthMvp.People.create_session(person)
+
+        person ->
+          {:ok, p} = AuthMvp.People.update_person(person, %{avatar: profile.avatar_url})
+          AuthMvp.People.create_session(p)
+      end
+
+    person = AuthMvp.People.get_person_by_email(profile.email)
+
+    jwt =
+      AuthMvp.Token.generate_and_sign!(%{
+        email: person.email,
+        session: session.id,
+        avatar: person.avatar
+      })
+
     redirect(conn, external: "#{client}?jwt=#{jwt}")
   end
 end
